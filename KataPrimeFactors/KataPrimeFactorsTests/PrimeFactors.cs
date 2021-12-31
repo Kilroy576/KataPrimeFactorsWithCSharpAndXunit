06dcb82849f41e5ab698ae92654c9fe33e4d9dd9
Ralf Schoch <ralf.schoch@codeso.de>
2021-12-31 19:11:14 +0100

    Make first test green. needs refactoring

---
+++ KataPrimeFactors/KataPrimeFactorsTests/PrimeFactors.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace KataPrimeFactorsTests
+{
+    public class PrimeFactors   
+    {   
+        public List<int> Generate(int input)
+        {
+            return new List<int>() { 2 };
+        }
+    }
+}
