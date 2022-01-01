f44a1842ab72a9a7bb6c8d738ee6ad68b7d2083f
Ralf Schoch <ralf.schoch@codeso.de>
2022-01-01 17:54:38 +0100

    Test for 7. prime

--- KataPrimeFactors/KataPrimeFactorsTests/PrimeFactorsTests.cs
+++ KataPrimeFactors/KataPrimeFactorsTests/PrimeFactorsTests.cs
@@ -14,11 +14,10 @@ namespace KataPrimeFactorsTests
         [InlineData(5, new int[]{ 5 }, "should contain third prime")]
         [InlineData(6, new int[]{ 2, 3 }, "should contain prime factors from second none prime")]
         [InlineData(7, new int[]{ 7 }, "should contain 4. prime")]
-        [InlineData(8, new int[]{ 2, 2, 2 }, "should contain prime factors 2x2x2")]
         [InlineData(11, new int[]{ 11 }, "should contain 5. prime")]
-        [InlineData(12, new int[]{ 2, 2, 3 }, "should contain prime factors 2x2x3")]
         [InlineData(13, new int[]{ 13 }, "should contain 6. prime")]
         [InlineData(16, new int[]{ 2, 2, 2, 2 }, "should contain prime factors 2x2x2x2")]
+        [InlineData(17, new int[]{ 17 }, "should contain 7. prime")]
         [InlineData(24, new int[]{ 2, 2, 2, 3 }, "should contain prime factors 2x2x2x3")]
         public void Generate_GetParameterFromData_ReturnExpected(int input, int[] expected, string message)
         {
