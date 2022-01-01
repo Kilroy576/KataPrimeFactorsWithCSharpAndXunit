9d7168c87f74f196a563230fffdc9155dd9c7f01
Ralf Schoch <ralf@schoch.com>
2022-01-01 17:08:20 +0100

    Add message to test

--- KataPrimeFactors/KataPrimeFactorsTests/PrimeFactorsTests.cs
+++ KataPrimeFactors/KataPrimeFactorsTests/PrimeFactorsTests.cs
@@ -8,16 +8,17 @@ namespace KataPrimeFactorsTests
     public class PrimeFactorsTests
     {
         [Theory]
-        [InlineData(2, new int[]{ 2 })]
-        [InlineData(3, new int[]{ 3 })]
-        [InlineData(4, new int[]{ 2, 2 })]
-        public void Generate_GetParameterFromData_ReturnExpected(int input, int[] expected)
+        [InlineData(2, new int[]{ 2 }, "should contain first prime")]
+        [InlineData(3, new int[]{ 3 }, "should contain second prime")]
+        [InlineData(4, new int[]{ 2, 2 }, "should contain prime factors from first none prime")]
+        [InlineData(5, new int[]{ 5 }, "should contain third prime")]
+        public void Generate_GetParameterFromData_ReturnExpected(int input, int[] expected, string message)
         {
             var target = new PrimeFactors();
 
             var actual = target.Generate(input);
 
-            actual.Should().Equal(expected);
+            actual.Should().Equal(expected, message);
         }
     }
 }
