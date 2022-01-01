e195df5cf7847e58b204b6d17cc2423f7cce76c0
Ralf Schoch <ralf.schoch@codeso.de>
2022-01-01 16:45:56 +0100

    Add test for first none prime

--- KataPrimeFactors/KataPrimeFactorsTests/PrimeFactorsTests.cs
+++ KataPrimeFactors/KataPrimeFactorsTests/PrimeFactorsTests.cs
@@ -10,6 +10,7 @@ namespace KataPrimeFactorsTests
         [Theory]
         [InlineData(2, new int[]{ 2 })]
         [InlineData(3, new int[]{ 3 })]
+        [InlineData(4, new int[]{ 2, 2 })]
         public void Generate_GetParameterFromData_ReturnExpected(int input, int[] expected)
         {
             var target = new PrimeFactors();
